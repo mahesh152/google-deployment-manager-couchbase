2c6ce59013afd2d9b24efce1eaa8deb1c0d3ef61
Mahesh Mandhare <mahesh.mandhare@couchbase.com>
2018-08-09 13:54:21 +0530

    fixing sed cmd for / and variables

--- simple/deploy.sh
+++ simple/deploy.sh
@@ -8,10 +8,10 @@ serverUrl=$5
 serverUser=$6
 serverPassword=$7
 
-sed -i 's/valServerUrl/${serverUrl}/g' parameters.${PARAMETERS_FILE}.yaml
-sed -i 's/valServerUser/${serverUser}/g' parameters.${PARAMETERS_FILE}.yaml
-sed -i 's/valServerPassword/${serverPassword}/g' parameters.${PARAMETERS_FILE}.yaml
-sed -i 's/valServerVersion/${serverVersion}/g' parameters.${PARAMETERS_FILE}.yaml
-sed -i 's/valNodeCount/${nodeCount}/g' parameters.${PARAMETERS_FILE}.yaml
+sed -i 's@valServerUrl@'"${serverUrl}"'@g' parameters.${PARAMETERS_FILE}.yaml
+sed -i 's@valServerUser@'"${serverUser}"'@g' parameters.${PARAMETERS_FILE}.yaml
+sed -i 's@valServerPassword@'"${serverPassword}"'@g' parameters.${PARAMETERS_FILE}.yaml
+sed -i 's@valServerVersion@'"${serverVersion}"'@g' parameters.${PARAMETERS_FILE}.yaml
+sed -i 's@valNodeCount@'"${nodeCount}"'@g' parameters.${PARAMETERS_FILE}.yaml
 
 #gcloud deployment-manager deployments create ${DEPLOYMENT_NAME} --config parameters.${PARAMETERS_FILE}.yaml
